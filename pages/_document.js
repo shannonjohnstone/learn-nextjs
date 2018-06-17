bdd95c5e4735b4e38bd1c8b060c0f1c4769f6c58
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-06-17 18:13:45 +1000

    add custom _document

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta name='viewport' content='width=device-width' />
+          <link rel="stylesheet" href="/static/styles.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
