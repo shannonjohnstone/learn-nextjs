9f9fbefc5fbccf514da07a1eb031429ca1fd740d
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-06-17 14:13:35 +1000

    add Navigation component

---
+++ components/navigation/Navigation.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const pages = ['Home', 'About', 'Contact'];
+
+const renderLinks = (page) => {
+  const link = page.toLowerCase()
+  return <Link href={link === 'home' ? 'index' : link}><a>{page}</a></Link>
+}
+
+const Navigation = (current) => (
+  <nav>{pages.filter(page => page !== current).map(renderLinks)}</nav>
+)
+
+export default Navigation;
