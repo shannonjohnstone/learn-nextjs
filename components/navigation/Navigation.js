eca1d1ac5eac6c4ab1f8e68f3afd2b6078210766
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-06-17 21:09:41 +1000

    update Navigation and contact components

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -2,13 +2,36 @@ import Link from 'next/link';
 
 const pages = ['Home', 'About', 'Contact'];
 
-const renderLinks = (page) => {
-  const link = page.toLowerCase()
-  return <Link key={link} href={link === 'home' ? 'index' : link}><a className='nav__item'>{page}</a></Link>
+const isRoot = page => page === 'home'
+const returnUrl = (page, isRoot) => isRoot ? '/' : page;
+const determineClassName = current => pageName => `nav__item ${(pageName === current) ? 'active' : ''}`
+
+const RenderLinks = ({ pageName, isActive, url }) => {
+  return (
+    <Link key={url} href={url}>
+      <a className={isActive}>{pageName}</a>
+    </Link>
+  )
 }
 
-const Navigation = (current) => (
-  <nav className='nav'>{pages.filter(page => page !== current).map(renderLinks)}</nav>
-)
+const Navigation = ({ current }) => {
+  const isActive = determineClassName(current);
+  return (
+    <nav className='nav'>
+      {
+        pages.map((pageName) => {
+          const pageNameLowerCase = pageName.toLowerCase()
+          return (
+            <RenderLinks
+              pageName={pageName}
+              isActive={isActive(pageNameLowerCase)}
+              url={returnUrl(pageNameLowerCase, isRoot(pageNameLowerCase))}
+            />
+          )
+        })
+      }
+    </nav>
+  )
+}
 
 export default Navigation;
