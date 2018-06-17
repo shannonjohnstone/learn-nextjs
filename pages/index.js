9fbda41b325bed3e5a21992da3a06d8b3bc6f13d
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-06-17 13:50:49 +1000

    add quick nav to index

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-export default () => <h1>Welcome!</h1>;
+export default () => (
+  <div>
+    <h1>Welcome!</h1>
+    <p>This site will a confernce</p>
+    <p>
+      <a href="/about">About</a>
+    </p>
+    <p>
+      <a href="/contact">Contact</a>
+    </p>
+  </div>
+);
