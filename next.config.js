dcf1e6a61e97fbc1b3457312de9e0f6291df050c
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-06-19 13:05:42 +1000

    impletment .env usage

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack')
+
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+})
+
+module.exports = {
+  webpack: config => {
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(process.env)
+    );
+    return config;
+  }
+};
